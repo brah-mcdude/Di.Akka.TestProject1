f88e28d4c9b8f5722a08ad998c465281eba886b4
Brah McDude <77924970+brah-mcdude@users.noreply.github.com>
2023-04-07 10:07:41 -0400

    2 tests;

---
+++ TestProject1/ConnectTest.cs
@@ -0,0 +1,15 @@
+using Xunit.Abstractions;
+
+namespace TestProject1
+{
+    public class ConnectTest : Di.DiSpecBase
+    {
+        public ConnectTest(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TestProject1/CreateTest.cs
@@ -0,0 +1,16 @@
+using Akka.Actor.Setup;
+using Xunit.Abstractions;
+
+namespace TestProject1
+{
+    public class CreateTest : SpecBase
+    {
+        public CreateTest(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
